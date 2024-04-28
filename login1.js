f048fc37fa474cc758064bf7e982d0d40e8ae0af
coding9787 <coding9787@gmail.com>
2024-04-28 17:43:06 +0530

    loginPageCompleted

--- login1.js
+++ login1.js
@@ -2,12 +2,22 @@
 const wrapper=document.querySelector(".wrapper");
 const loginlink=document.querySelector(".loginlink");
 const registerlink=document.querySelector(".registerlink");
+const btnpopup=document.querySelector(".buttonLogin");
+const closeIcon=document.querySelector(".closeIcon");
 registerlink.addEventListener('click',()=>{
     wrapper.classList.add('active');
     
 });
 loginlink.addEventListener('click',()=>{
     wrapper.classList.remove('active');
+    
+});
+btnpopup.addEventListener('click',()=>{
+    wrapper.classList.add('active-popup');
+    
+});
+closeIcon.addEventListener('click',()=>{
+    wrapper.classList.remove('active-popup');
     
 });
 
